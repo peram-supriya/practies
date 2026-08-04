47314e21b81b2e094107a2d157f006b5a138f432
peram-supriya <perampriya26@gmail.com>
2026-08-04 23:24:18 +0530

    actions

--- 04-message.sh
+++ 04-message.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%S)
 
-echo "The started time is $TIMESTAMP"
+echo "The started time is $START_TIME"
+
+END_TIME=$(date +%S)
+
+TOTAL_TIME=(($END_TIME-$START_TIME))
+
+echo "The ended time is $TOTAL_TIME"
