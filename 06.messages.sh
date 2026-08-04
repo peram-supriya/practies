56615a591bc24bbf16bfbd628bab8fea58f9fcb4
peram-supriya <perampriya26@gmail.com>
2026-08-04 23:55:50 +0530

    actions

---
+++ 06.messages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITES=("apples" "ornges" "mangoes")
+
+echo "fruits are ${FRUITES[@]}"
+echo "First fruits ${FRUITES[0]}"
+echo "Second fruits ${FRIUTES[1]}"
+echo "Tird fruite ${FRUITES[2]}"
