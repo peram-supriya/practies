3b7200d3cd004c45625f383716fd2fb9839bf737
peram-supriya <perampriya26@gmail.com>
2026-08-05 17:20:20 +0530

    actions

---
+++ 13.loop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_folder="/var/log/practies"
+LOG_FILE="/var/log/practies/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Running as root user" | tee -a $LOG_FILE
+    exit 1
+
+
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 failed" | tee -a $LOG_FILE
+        exit 1
+
+    else
+        echo "$2 successful" | tee -a $LOG_FILE
+fi
+}
+
+for package in $@
+do 
+    dnf install $package -y &>> $LOG_FILE
+    validate $? "$package installation"
+done
